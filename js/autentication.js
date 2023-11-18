d92d82f93471dacf5003fea6d7bd103f081dcad8
Niku <armandosempai10@gmail.com>
2023-11-18 14:14:49 -0500

    Inicio de sesion y formulario de adopcion

--- js/autentication.js
+++ js/autentication.js
@@ -1,9 +1,12 @@
 function registerUser() {
+    const name = document.getElementById('registerName').value;
+    const lastName = document.getElementById('registerLastname').value;
     const email = document.getElementById('registerEmail').value;
     const password = document.getElementById('registerPassword').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
+    const gender = document.getElementById('registerGender').value;
 
-    if (!email || !password || !confirmPassword) {
+    if (!name || !lastName || !gender || !email || !password || !confirmPassword) {
         alert('Por favor, complete todos los campos');
         return;
     }
@@ -13,7 +16,7 @@ function registerUser() {
         return;
     }
 
-    const user = { email, password };
+    const user = { name, lastName, gender, email, password };
     localStorage.setItem('user', JSON.stringify(user));
 
     alert('Usuario registrado correctamente');
@@ -31,10 +34,40 @@ function loginUser() {
     const storedUser = JSON.parse(localStorage.getItem('user'));
 
     if (storedUser && storedUser.email === email && storedUser.password === password) {
-        alert('Inicio de sesión exitoso');
+        console.log('Inicio de sesión exitoso');
+        sessionStorage.setItem('loggedInUser', JSON.stringify(storedUser));
+        window.location.href = 'index.html'; 
     } else {
-        alert('Nombre de usuario o contraseña incorrectos');
+        console.error('Nombre de usuario o contraseña incorrectos');
+        alert('Usuario no registrado. Por favor, regístrate antes de iniciar sesión.');
     }
+}
+
+function logoutUser() {
+    sessionStorage.removeItem('loggedInUser');
+    updateNavigation();
+    location.reload();
+}
+
+function updateNavigation() {
+    const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+
+    if (loggedInUser) {
+        const navigationMenu = document.getElementById('navigationMenu');
+        navigationMenu.innerHTML = `
+            <span>Bienvenido, ${loggedInUser.name}!</span>
+            <button onclick="logoutUser()">Cerrar sesión</button>
+        `;
+    } else {
+        const navigationMenu = document.getElementById('navigationMenu');
+        navigationMenu.innerHTML = `
+            <button onclick="showLoginForm()">Iniciar sesión</button>
+        `;
+    }
+}
+
+function showLoginForm() {
+    window.location.href = 'login.html';
 }
 
 function goToHomePage() {
